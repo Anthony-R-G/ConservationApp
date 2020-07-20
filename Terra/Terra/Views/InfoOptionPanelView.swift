058eeedc64ae6af460b0d793a10fceaadb0802f1
AGonzalez5195 <AnthonyGonzalez@pursuit.org>
2020-07-20 16:12:50 -0400

    Removed unused code from infoOptionPanelView

--- Terra/Terra/Views/InfoOptionPanelView.swift
+++ Terra/Terra/Views/InfoOptionPanelView.swift
@@ -81,39 +81,23 @@ extension InfoOptionPanelView {
         let path = UIBezierPath()
         let centerWidth = self.frame.width / 2
         
-        path.move(to: CGPoint(x: 0, y: 0)) // start top left
-        path.addLine(to: CGPoint(x: (centerWidth - height * 2), y: 0)) // the beginning of the trough
+        path.move(to: CGPoint(x: 0, y: 0)) // Starts from top left of self
+        path.addLine(to: CGPoint(x: (centerWidth - height * 2), y: 0)) // Trough beginning
         
-        // first curve down
+        // First curve (going down))
         path.addCurve(to: CGPoint(x: centerWidth, y: height),
                       controlPoint1: CGPoint(x: (centerWidth - 40), y: 0), controlPoint2: CGPoint(x: centerWidth - 45, y: height))
-        // second curve up
+        
+        // Second curve (going up)
         path.addCurve(to: CGPoint(x: (centerWidth + height * 2), y: 0),
                       controlPoint1: CGPoint(x: centerWidth + 45, y: height), controlPoint2: CGPoint(x: (centerWidth + 40), y: 0))
         
-        // complete the rect
+        // Complete the rectangle
         path.addLine(to: CGPoint(x: self.frame.width, y: 0))
         path.addLine(to: CGPoint(x: self.frame.width, y: self.frame.height))
         path.addLine(to: CGPoint(x: 0, y: self.frame.height))
         path.close()
         
-        return path.cgPath
-    }
-    
-
-    func createPathCircle() -> CGPath {
-        
-        let radius: CGFloat = 37.0
-        let path = UIBezierPath()
-        let centerWidth = self.frame.width / 2
-        
-        path.move(to: CGPoint(x: 0, y: 0))
-        path.addLine(to: CGPoint(x: (centerWidth - radius * 2), y: 0))
-        path.addArc(withCenter: CGPoint(x: centerWidth, y: 0), radius: radius, startAngle: CGFloat(180).degreesToRadians, endAngle: CGFloat(0).degreesToRadians, clockwise: false)
-        path.addLine(to: CGPoint(x: self.frame.width, y: 0))
-        path.addLine(to: CGPoint(x: self.frame.width, y: self.frame.height))
-        path.addLine(to: CGPoint(x: 0, y: self.frame.height))
-        path.close()
         return path.cgPath
     }
 }
