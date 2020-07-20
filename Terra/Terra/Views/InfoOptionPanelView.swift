93521f76f8656f1e4e8afbf871e0eb8081d6b2df
AGonzalez5195 <AnthonyGonzalez@pursuit.org>
2020-07-20 01:28:23 -0400

    Created file for the panel that will be underneath the middle view on the detail species vc. This panel will have individual elements that can be selected which will change what information is presented in the middle view.

---
+++ Terra/Terra/Views/InfoOptionPanelView.swift
@@ -0,0 +1,21 @@
+//
+//  InfoOptionPanelView.swift
+//  Terra
+//
+//  Created by Anthony Gonzalez on 7/20/20.
+//  Copyright © 2020 Antnee. All rights reserved.
+//
+
+import UIKit
+
+class InfoOptionPanelView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
