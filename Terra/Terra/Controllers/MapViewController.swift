e44faabce5750f69cb1b6ae2cb4065f07caf32a1
AGonzalez5195 <AnthonyGonzalez@pursuit.org>
2020-07-30 00:58:04 -0400

    Added file for Map VC

---
+++ Terra/Terra/Controllers/MapViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MapViewController.swift
+//  Terra
+//
+//  Created by Anthony Gonzalez on 7/30/20.
+//  Copyright © 2020 Antnee. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
