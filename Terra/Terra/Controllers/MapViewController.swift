ba10efc1832b01f90291c6628f852f73f2a9c987
AGonzalez5195 <AnthonyGonzalez@pursuit.org>
2020-07-30 14:04:13 -0400

    Added title/subtitle optional arguments to makeMKAnnotation method in Map VC

--- Terra/Terra/Controllers/MapViewController.swift
+++ Terra/Terra/Controllers/MapViewController.swift
@@ -11,41 +11,51 @@ import MapKit
 
 class MapViewController: UIViewController {
     
+    //MARK: -- UI Element Initialization
     private lazy var mapView: MKMapView = {
         let mv = MKMapView()
-        mv.mapType = .satellite
+        mv.mapType = .satelliteFlyover
+        mv.showsTraffic = false
         mv.delegate = self
         return mv
     }()
     
+    //MARK: -- Properties
     var habitatLocation = CLLocation() {
         didSet {
-            addMapAnnotation(latitude: habitatLocation.coordinate.latitude, longitude: habitatLocation.coordinate.longitude)
+            makeMKAnnotation(latitude: habitatLocation.coordinate.latitude, longitude: habitatLocation.coordinate.longitude, title: "Amur Leopard", subtitle: nil)
             mapView.centerToLocation(habitatLocation)
         }
     }
     
-    func makeCLLocation(latitude: Double, longitude: Double) -> CLLocation {
+    //MARK: -- Methods
+    func makeCLLocation(latitude: CLLocationDegrees, longitude: CLLocationDegrees) -> CLLocation {
         let location = CLLocation(latitude: latitude, longitude: longitude)
         return location
     }
     
     
-    private func addMapAnnotation(latitude: CLLocationDegrees, longitude: CLLocationDegrees) {
+    private func makeMKAnnotation(latitude: CLLocationDegrees, longitude: CLLocationDegrees, title: String?, subtitle: String?) {
         let annotation = MKPointAnnotation()
         annotation.coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
         mapView.addAnnotation(annotation)
+        if let title = title {
+            annotation.title = title
+        }
+        if let subtitle = subtitle {
+            annotation.subtitle = subtitle
+        }
+        
     }
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         addSubviews()
         setConstraints()
-        
     }
 }
 
+//MARK: -- MapView Delegate Methods
 extension MapViewController: MKMapViewDelegate {
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         guard annotation is MKPointAnnotation else { return nil }
@@ -55,7 +65,6 @@ extension MapViewController: MKMapViewDelegate {
         
         if annotationView == nil {
             annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
-            
             annotationView!.canShowCallout = true
         } else {
             annotationView!.annotation = annotation
@@ -63,12 +72,16 @@ extension MapViewController: MKMapViewDelegate {
         
         return annotationView
     }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        print(view.annotation?.title)
+    }
 }
 
 private extension MKMapView {
     func centerToLocation(
         _ location: CLLocation,
-        regionRadius: CLLocationDistance = 1000
+        regionRadius: CLLocationDistance = 200000
     ) {
         let coordinateRegion = MKCoordinateRegion(
             center: location.coordinate,
