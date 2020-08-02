81a58f03f6dbffd71eb0a5a00be24397db9108a9
AGonzalez5195 <AnthonyGonzalez@pursuit.org>
2020-08-02 16:48:52 -0400

    Changed entry VC in SceneDelegate. Added NewsViewController file

---
+++ Terra/Terra/Controllers/NewsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  NewsViewController.swift
+//  Terra
+//
+//  Created by Anthony Gonzalez on 8/2/20.
+//  Copyright © 2020 Antnee. All rights reserved.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- Terra/Terra/Controllers/TabBarController.swift
+++ Terra/Terra/Controllers/TabBarController.swift
@@ -9,4 +9,33 @@
 import Foundation
 import FloatingTabBarController
 
-
+class FloatingTabViewController: FloatingTabBarController {
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    private func setTabItemIcons() {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        tabBar.visualEffect = UIBlurEffect(style: .systemUltraThinMaterialDark)
+        tabBar.tintColor = .white
+        
+        let firstVC = SpeciesListViewController()
+        let leopardSmallIcon = UIImage(named: "leopardSmall")!
+        let leopardLargeIcon = UIImage(named: "leopardLarge")!
+        firstVC.floatingTabItem = FloatingTabItem(selectedImage: leopardLargeIcon, normalImage: leopardSmallIcon)
+        
+        
+        let secondVC = NewsViewController()
+        let newsSmallIcon = UIImage(named: "newsSmall")!
+        let newsLargeIcon = UIImage(named: "newsLarge")!
+        secondVC.floatingTabItem = FloatingTabItem(selectedImage: newsLargeIcon, normalImage: newsSmallIcon)
+        
+        viewControllers = [firstVC, secondVC ]
+    }
+}
