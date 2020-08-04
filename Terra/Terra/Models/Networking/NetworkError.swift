cc61a3197f6af1d1d1da89386d0822c05f762d8a
AGonzalez5195 <AnthonyGonzalez@pursuit.org>
2020-08-04 18:04:19 -0400

    Tweaked network manager to remove unneeded lines

--- Terra/Terra/Models/Networking/NetworkError.swift
+++ Terra/Terra/Models/Networking/NetworkError.swift
@@ -9,13 +9,10 @@
 import Foundation
 
 enum NetworkError: Error {
-    case unauthenticated
-    case invalidJSONResponse
     case couldNotParseJSON(rawError: Error)
     case noInternetConnection
     case badURL
     case badStatusCode
     case noDataReceived
-    case notAnImage
     case other(rawError: Error)
 }
