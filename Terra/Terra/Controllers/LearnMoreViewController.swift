cbd8b1725e4555707a0e8394c0447351acc92e40
Anthony-R_G <anthonygonzalez@pursuit.org>
2020-08-10 18:13:27 -0400

    Added file for learn more vc

---
+++ Terra/Terra/Controllers/LearnMoreViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LearnMoreViewController.swift
+//  Terra
+//
+//  Created by Anthony Gonzalez on 8/10/20.
+//  Copyright © 2020 Antnee. All rights reserved.
+//
+
+import UIKit
+
+class LearnMoreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
