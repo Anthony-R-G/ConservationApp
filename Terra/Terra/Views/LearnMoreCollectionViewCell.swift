e207a713070f80edd7f8236267507b4bcc4454e4
Anthony-R_G <anthonygonzalez@pursuit.org>
2020-08-11 15:23:41 -0400

    Added file for learn more cell

---
+++ Terra/Terra/Views/LearnMoreCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  LearnMoreCollectionViewCell.swift
+//  Terra
+//
+//  Created by Anthony Gonzalez on 8/11/20.
+//  Copyright © 2020 Antnee. All rights reserved.
+//
+
+import UIKit
+
+class LearnMoreCollectionViewCell: UICollectionViewCell {
+    
+}
