bda17e076cd218c530d59a40db418d94bff9c796
Anthony-R_G <anthonygonzalez@pursuit.org>
2020-08-11 04:33:43 -0400

    Added labels to headerview

--- Terra/Terra/Views/CollectionViewHeader.swift
+++ Terra/Terra/Views/CollectionViewHeader.swift
@@ -45,6 +45,32 @@ class CollectionViewHeader: UICollectionReusableView {
         return UIVisualEffectView(effect: UIBlurEffect(style: .regular))
     }()
     
+    private lazy var titleLabel: UILabel = {
+        return Factory.makeLabel(title: "Amur Leopard",
+                                 weight: .bold,
+                                 size: 25,
+                                 color: .white,
+                                 alignment: .left)
+    }()
+    
+    private lazy var subtitleLabel: UILabel = {
+        return Factory.makeLabel(title: "Panthers pardus orientalis",
+                                 weight: .regular,
+                                 size: 14,
+                                 color: .white,
+                                 alignment: .left)
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [
+            titleLabel, subtitleLabel
+        ])
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        
+        return stackView
+    }()
+    
     //MARK: -- Properties
     var animator: UIViewPropertyAnimator!
     
@@ -64,14 +90,44 @@ class CollectionViewHeader: UICollectionReusableView {
         animator.fractionComplete = 0
     }
     
+    private func setupGradientLayer() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [UIColor.clear.cgColor, UIColor.black.cgColor]
+        gradientLayer.locations = [0.5, 1]
+        
+        let gradientContainerView = UIView()
+        addSubview(gradientContainerView)
+        gradientContainerView.snp.makeConstraints {[weak self] (make) in
+            guard let self = self else { return }
+            make.leading.bottom.trailing.equalTo(self)
+        }
+        gradientContainerView.layer.addSublayer(gradientLayer)
+        gradientLayer.frame = self.bounds
+        gradientLayer.frame.origin.y -= bounds.height
+        
+        
+        let stackView = UIStackView(arrangedSubviews: [
+            titleLabel, subtitleLabel
+        ])
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        addSubview(stackView)
+        stackView.snp.makeConstraints { [weak self ](make) in
+            guard let self = self else { return }
+            make.leading.bottom.trailing.equalTo(self).inset(20)
+        }
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .black
         
-        addSubviews()
-        setConstraints()
+        addSubview(headerImageView)
+        headerImageView.addSubview(headerGradient)
         
         setupVisualEffectBlur()
+        setupGradientLayer()
+        setConstraints()
     }
     
     required init?(coder: NSCoder) {
@@ -83,15 +139,10 @@ class CollectionViewHeader: UICollectionReusableView {
 
 fileprivate extension CollectionViewHeader {
     
-    func addSubviews() {
-        addSubview(headerImageView)
-        headerImageView.addSubview(headerGradient)
-    }
     
     func setConstraints() {
         setHeaderImageViewConstraints()
         setHeaderGradientConstraints()
-        setBottomGradientConstraints()
     }
     
     
@@ -107,11 +158,5 @@ fileprivate extension CollectionViewHeader {
             make.height.equalTo(headerImageView.snp.height).multipliedBy(0.25)
         }
     }
-    
-    func setBottomGradientConstraints() {
-        bottomGradient.snp.makeConstraints {[weak self] (make) in
-            guard let self = self else { return }
-            make.leading.bottom.trailing.equalTo(self)
-        }
-    }
 }
+
