05c6d4ea9db7942265918df979aa3232b528c96c
Anthony-R_G <anthonygonzalez@pursuit.org>
2020-08-15 20:14:50 -0400

    Added animatable file

--- Terra/Terra/Third Party Code/Animatable.swift
+++ Terra/Terra/Third Party Code/Animatable.swift
@@ -1,9 +1,37 @@
-//
-//  Animatable.swift
-//  Terra
-//
-//  Created by Anthony Gonzalez on 8/15/20.
-//  Copyright © 2020 Antnee. All rights reserved.
-//
+import UIKit
 
-import Foundation
+protocol Animatable {
+    var containerView: UIView? { get }
+    var childView: UIView? { get }
+
+    func presentingView(
+        sizeAnimator: UIViewPropertyAnimator,
+        positionAnimator: UIViewPropertyAnimator,
+        fromFrame: CGRect,
+        toFrame: CGRect
+    )
+
+    func dismissingView(
+        sizeAnimator: UIViewPropertyAnimator,
+        positionAnimator: UIViewPropertyAnimator,
+        fromFrame: CGRect,
+        toFrame: CGRect
+    )
+}
+
+/// Default implementations
+extension Animatable {
+    func presentingView(
+        sizeAnimator: UIViewPropertyAnimator,
+        positionAnimator: UIViewPropertyAnimator,
+        fromFrame: CGRect,
+        toFrame: CGRect
+    ) {}
+
+    func dismissingView(
+        sizeAnimator: UIViewPropertyAnimator,
+        positionAnimator: UIViewPropertyAnimator,
+        fromFrame: CGRect,
+        toFrame: CGRect
+    ) {}
+}
