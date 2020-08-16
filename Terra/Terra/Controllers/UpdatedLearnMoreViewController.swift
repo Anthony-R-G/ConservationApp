eb58b9491df8ff9fa5fd59382f0197da9c1a9656
Anthony-R_G <anthonygonzalez@pursuit.org>
2020-08-15 20:18:20 -0400

    Added Animatable extension to LearnMoreVC

--- Terra/Terra/Controllers/UpdatedLearnMoreViewController.swift
+++ Terra/Terra/Controllers/UpdatedLearnMoreViewController.swift
@@ -177,3 +177,94 @@ extension UpdatedLearnMoreViewController {
         }
     }
 }
+
+//MARK: -- Animatable Methods
+extension UpdatedLearnMoreViewController: Animatable {
+    var containerView: UIView? {
+        return view
+    }
+    
+    var childView: UIView? {
+        return commonView
+    }
+    
+    func presentingView(
+        sizeAnimator: UIViewPropertyAnimator,
+        positionAnimator: UIViewPropertyAnimator,
+        fromFrame: CGRect,
+        toFrame: CGRect
+    ) {
+        // Make the common view the same size as the initial frame
+        heightConstraint.constant = fromFrame.height
+        
+        // Show the close button
+        closeButton.alpha = 1
+        
+        // Make the view look like a card
+        asCard(true)
+        
+        // Redraw the view to update the previous changes
+        view.layoutIfNeeded()
+        
+        // Push the content of the common view down to stay within the safe area insets
+        let safeAreaTop = UIApplication.shared.connectedScenes
+            .filter({$0.activationState == .foregroundActive})
+            .map({$0 as? UIWindowScene})
+            .compactMap({$0})
+            .first?.windows
+            .filter({$0.isKeyWindow}).first?.safeAreaInsets.top ?? .zero
+        commonView.topConstraintValue = safeAreaTop + 16
+        
+        // Animate the common view to a height of 500 points
+        heightConstraint.constant = 500
+        sizeAnimator.addAnimations { [weak self] in
+            guard let self = self else { return }
+            self.view.layoutIfNeeded()
+        }
+        
+        // Animate the view to not look like a card
+        positionAnimator.addAnimations { [weak self] in
+            guard let self = self else { return }
+            self.asCard(false)
+        }
+    }
+    
+    func dismissingView(
+        sizeAnimator: UIViewPropertyAnimator,
+        positionAnimator: UIViewPropertyAnimator,
+        fromFrame: CGRect,
+        toFrame: CGRect
+    ) {
+        // If the user has scrolled down in the content, force the common view to go to the top of the screen.
+        topConstraint.isActive = true
+        
+        // If the top card is completely off screen, we move it to be JUST off screen.
+        // This makes for a cleaner looking animation.
+        if scrollView.contentOffset.y > commonView.frame.height {
+            topConstraint.constant = -commonView.frame.height
+            view.layoutIfNeeded()
+            
+            // Still want to animate the common view getting pinned to the top of the view
+            topConstraint.constant = 0
+        }
+        
+        // Common view does not need to worry about the safe area anymore. Just restore the original value.
+        commonView.topConstraintValue = 16
+        
+        // Animate the height of the common view to be the same size as the TO frame.
+        // Also animate hiding the close button
+        heightConstraint.constant = toFrame.height
+        sizeAnimator.addAnimations { [weak self] in
+            guard let self = self else { return }
+            self.closeButton.alpha = 0
+            self.view.layoutIfNeeded()
+        }
+        
+        // Animate the view to look like a card
+        positionAnimator.addAnimations { [weak self] in
+            guard let self = self else { return }
+            self.asCard(true)
+        }
+    }
+}
+
