20c795c094d91a93ab47bbd10e14e5a0c7025d4b
Anthony-R_G <anthonygonzalez@pursuit.org>
2020-08-15 20:19:36 -0400

    Added nav delegate for custom transition

--- Terra/Terra/Controllers/NavigationController.swift
+++ Terra/Terra/Controllers/NavigationController.swift
@@ -16,7 +16,18 @@ class NavigationController: UINavigationController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        delegate = self
+    }
+}
+
+extension NavigationController: UINavigationControllerDelegate {
+    func navigationController(
+        _ navigationController: UINavigationController,
+        animationControllerFor operation: UINavigationController.Operation,
+        from fromVC: UIViewController,
+        to toVC: UIViewController
+    ) -> UIViewControllerAnimatedTransitioning? {
+        return CustomTransitionAnimation(operation: operation, positioningDuration: 1, resizingDuration: 0.5)
     }
 }
 
